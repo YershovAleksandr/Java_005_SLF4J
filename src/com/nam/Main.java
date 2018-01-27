f1b91923d4d7bc5af58b07f5d655259383231cc9
hp <namstudionsk@gmail.com>
2018-01-28 02:42:53 +0700

    Add slf4j all files and create hello world

--- src/com/nam/Main.java
+++ src/com/nam/Main.java
@@ -1,8 +1,24 @@
 package com.nam;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+
+        Logger logger = LoggerFactory.getLogger(Main.class);
+
+        logger.info("Hello World");
+
+        logger.debug("Wtf");
+
+        logger.warn("Fuck");
+
+        logger.error("42");
+
+        logger.trace("111");
+
     }
 }
